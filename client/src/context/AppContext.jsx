3257761383942a62d0ae2d3e54f5f44cfb086167
Pallvi Saini <pallvis3008@gmail.com>
2025-08-27 10:40:32 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext()
 export const AppContextProvider = (props)=>{
 
       axios.defaults.withCredentials = true;
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = 'https://auth-verification-mern.onrender.com'
     const [isLoggedin , setIsLoggedin] = useState(false)
     const [userData , setUserData] = useState(false)
 
